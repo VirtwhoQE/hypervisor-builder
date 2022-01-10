846e41cf479b3939ded8d0ec60f1f83a895aeb75
hkx303 <kuhuang@redhat.com>
2022-01-10 11:57:25 +0800

    add kubevirt guest_search function

--- hypervisor/virt/kubevirt/kubevirtapi.py
+++ hypervisor/virt/kubevirt/kubevirtapi.py
@@ -95,3 +95,78 @@ class KubevirtApi:
         :return:
         """
         return self._request('/apis/kubevirt.io/' + self._kubevirt_version() + '/virtualmachineinstances')
+
+    def get_nodes_list(self):
+        """
+        Returns the list for the node name.
+        :return: list of node's name.
+        """
+        hosts = []
+        nodes = self.get_nodes()
+        for node in nodes['items']:
+            name = node['metadata']['name']
+            hosts.append(name)
+        logger.debug(f"node list: {hosts}")
+        return hosts
+
+    def parse_cpu(self, cpu):
+        """
+        Parse the cpu to normal value
+        :param cpu: original value for the cpu
+        :return: the cpu after the convert
+        """
+        if cpu.endswith('m'):
+            cpu = int(math.floor(int(cpu[:-1]) / 1000))
+        return str(cpu)
+
+    def get_host_info(self, node_name):
+        """
+        Returns the messages for the host.
+        :param node_name: the name for the specific node
+        :return: return the host info include the host uuid, cpu, version and hostname
+        """
+        nodes = self.get_nodes()
+        host_info = {}
+        for node in nodes['items']:
+            if node['metadata']['name'] == node_name:
+                host_info['uuid'] = node['status']['nodeInfo']['machineID']
+                host_info['cpu'] = self.parse_cpu(node['status']['allocatable']["cpu"])
+                host_info['version'] = node['status']['nodeInfo']['kubeletVersion']
+                for addr in node['status']['addresses']:
+                    if addr['type'] == 'Hostname':
+                        host_info['hostname'] = addr['address']
+        logger.debug(f"host info: {host_info}")
+        return host_info
+
+    def get_vm_info(self, guest_name):
+        """
+        Returns the messages for the virtual manager.
+        :param guest_name: the guest name for the specific guest
+        :return: the guest messages for the virtual manger, include guest uuid, state,
+        nodename, etc.
+        """
+        vms = self.get_vms()
+        guest_info = {}
+        for vm in vms['items']:
+            if vm['metadata']['name'] == guest_name:
+                guest_info['guest_uuid'] = vm['spec']['domain']['firmware']['uuid']
+                guest_info['hostname'] = vm['status']['nodeName']
+                guest_info['guest_state'] = vm['status']['phase']
+        logger.debug(f"vm info: {guest_info}")
+        return guest_info
+
+    def guest_search(self, guest_name, guest_port):
+        """
+        Search the specific guest, return the expected attributes
+        Args:
+            guest_name (string) : name for the specific guest
+            guest_port (int) : port for the specific guest
+        Returns:
+            Vm info (dict): guest attributes, include guest_name, guest_uuid, guest_state,
+            uuid, hostname, version, cpu, etc.
+        """
+        guest_msgs = {}
+        guest_msgs.update(self.get_vm_info(guest_name))
+        guest_msgs['guest_ip'] = f"{guest_msgs['hostname']}:{guest_port}"
+        guest_msgs.update(self.get_host_info(guest_msgs['hostname']))
+        return guest_msgs
