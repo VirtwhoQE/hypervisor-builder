199b295d778c0f5ccf84fd2de904db4816b111f7
James Molet <jmolet@redhat.com>
2024-12-13 15:03:47 -0500

    putting this project into a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='hypervisor-builder',
+    version='0.1',
+    packages=['hypervisor', 'hypervisor.virt', 'hypervisor.virt.ahv', 'hypervisor.virt.esx', 'hypervisor.virt.xen', 'hypervisor.virt.rhevm', 'hypervisor.virt.hyperv', 'hypervisor.virt.libvirt', 'hypervisor.virt.kubevirt'],
+    url='https://github.com/VirtwhoQE/hypervisor-builder',
+    license='GPL-3.0',
+    author='',
+    author_email='',
+    description='Library to set up various hypervisors for virt-who testing.'
+)
